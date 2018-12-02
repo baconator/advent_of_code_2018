ed1798b07dd4681330b7eb296caf845283ca37d3
Bacon <baconator@users.noreply.github.com>
2018-12-01 21:56:56 -0800

    Used paths properly.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,25 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::path::Path;
 
 mod challenge1;
 mod challenge2;
 
 fn main() -> Result<(), std::io::Error> {
     {
-        let easy_lines = BufReader::new(File::open("F:\\Projects\\advent_of_code_2018\\inputs\\1a")?).lines().map(|line| line.unwrap());
-        println!("A: {}", challenge1::easy::run(easy_lines));
+        let easy_lines = BufReader::new(File::open(Path::new("./inputs/1a"))?).lines().map(|line| line.unwrap());
+        println!("D1 Easy: {}", challenge1::easy::run(easy_lines));
 
-        let hard_lines = BufReader::new(File::open("F:\\Projects\\advent_of_code_2018\\inputs\\1b")?).lines().map(|line| line.unwrap());
-        println!("B: {}", challenge1::hard::solve(hard_lines));
+        let hard_lines = BufReader::new(File::open(Path::new("./inputs/1a"))?).lines().map(|line| line.unwrap());
+        println!("D1 Hard: {}", challenge1::hard::solve(hard_lines));
     }
 
-    let easy_lines = BufReader::new(File::open("F:\\Projects\\advent_of_code_2018\\inputs\\2a")?).lines().map(|line| line.unwrap());
-    println!("A: {}", challenge2::easy::solve(easy_lines));
+    {
+        let easy_lines = BufReader::new(File::open(Path::new("./inputs/2a"))?).lines().map(|line| line.unwrap());
+        println!("D2 Easy: {}", challenge2::easy::solve(easy_lines));
 
-    let hard_lines = BufReader::new(File::open("F:\\Projects\\advent_of_code_2018\\inputs\\2a")?).lines().map(|line| line.unwrap());
-    println!("A: {}", challenge2::hard::solve(hard_lines));
+        let hard_lines = BufReader::new(File::open(Path::new("./inputs/2a"))?).lines().map(|line| line.unwrap());
+        println!("D2 Hard: {}", challenge2::hard::solve(hard_lines));
+    }
     Ok(())
 }
