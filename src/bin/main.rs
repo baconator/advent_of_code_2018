64d0d28695ab1c65600be9d3a437a70328f47cf9
Bacon <baconator@users.noreply.github.com>
2018-12-02 11:50:17 -0800

    Factored out a library.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,25 +1,25 @@
+extern crate aoc2018;
+
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
 
-mod challenge1;
-mod challenge2;
 
 fn main() -> Result<(), std::io::Error> {
     {
         let easy_lines = BufReader::new(File::open(Path::new("./inputs/1a"))?).lines().map(|line| line.unwrap());
-        println!("D1 Easy: {}", challenge1::easy::solve(easy_lines));
+        println!("D1 Easy: {}", aoc2018::challenge1::easy::solve(easy_lines));
 
         let hard_lines = BufReader::new(File::open(Path::new("./inputs/1a"))?).lines().map(|line| line.unwrap());
-        println!("D1 Hard: {}", challenge1::hard::solve(hard_lines));
+        println!("D1 Hard: {}", aoc2018::challenge1::hard::solve(hard_lines));
     }
 
     {
         let easy_lines = BufReader::new(File::open(Path::new("./inputs/2a"))?).lines().map(|line| line.unwrap());
-        println!("D2 Easy: {}", challenge2::easy::solve(easy_lines));
+        println!("D2 Easy: {}", aoc2018::challenge2::easy::solve(easy_lines));
 
         let hard_lines = BufReader::new(File::open(Path::new("./inputs/2a"))?).lines().map(|line| line.unwrap());
-        println!("D2 Hard: {}", challenge2::hard::solve(hard_lines));
+        println!("D2 Hard: {}", aoc2018::challenge2::hard::solve(hard_lines));
     }
     Ok(())
 }
