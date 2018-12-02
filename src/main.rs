18af771ef1385eca444227d6372b1200ca6f9474
Bacon <baconator@users.noreply.github.com>
2018-12-01 21:31:50 -0800

    Day 2, easy challenge.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 use std::fs::File;
-use std::io::{BufReader, BufRead};
+use std::io::{BufRead, BufReader};
 
 mod challenge1;
+mod challenge2;
 
 fn main() -> Result<(), std::io::Error> {
-    let easy_lines = BufReader::new(File::open("F:\\Projects\\advent_of_code_2018\\inputs\\1a")?).lines().map(|line| line.unwrap());
-    println!("A: {}", challenge1::easy::run(easy_lines));
+    {
+        let easy_lines = BufReader::new(File::open("F:\\Projects\\advent_of_code_2018\\inputs\\1a")?).lines().map(|line| line.unwrap());
+        println!("A: {}", challenge1::easy::run(easy_lines));
 
-    let hard_lines = BufReader::new(File::open("F:\\Projects\\advent_of_code_2018\\inputs\\1b")?).lines().map(|line| line.unwrap());
-    println!("B: {}", challenge1::hard::solve(hard_lines));
+        let hard_lines = BufReader::new(File::open("F:\\Projects\\advent_of_code_2018\\inputs\\1b")?).lines().map(|line| line.unwrap());
+        println!("B: {}", challenge1::hard::solve(hard_lines));
+    }
+
+    let easy_lines = BufReader::new(File::open("F:\\Projects\\advent_of_code_2018\\inputs\\2a")?).lines().map(|line| line.unwrap());
+    println!("A: {}", challenge2::easy::solve(easy_lines));
     Ok(())
 }
