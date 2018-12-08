950f6d4fceebee037d545e37f648ea9ccaadb1ee
Bacon <baconator@users.noreply.github.com>
2018-12-08 10:46:01 -0800

    ... not sure why this isn't working.

--- src/challenge6/easy.rs
+++ src/challenge6/easy.rs
@@ -20,6 +20,11 @@ fn test_d6e() {
     }
 }
 
+#[derive(PartialEq)]
+#[derive(Eq)]
+#[derive(Hash)]
+#[derive(Clone)]
+#[derive(Debug)]
 struct Position {
     x: usize,
     y: usize,
@@ -42,21 +47,28 @@ pub fn solve(lines: impl Iterator<Item = String>) -> usize {
     let width = (coords.iter().map(|p| p.x).max().unwrap() + 1) as i32;
     let height = (coords.iter().map(|p| p.y).max().unwrap() + 1) as i32;
 
-//    let mut position_to_count = HashMap::new();
-//    let mut infinite_positions = HashSet::new();
+    let mut id_to_count = HashMap::new();
+    let mut infinite_ids = HashSet::new();
 
-    for x in (-1..(width+1)) {
-        for y in (-1..(height+1)) {
-            let mut distances = coords
-                .iter()
-                .map(|c| (c.id, (x-(c.x as i32)).abs() + (y-(c.y as i32)).abs()))
-                .collect::<Vec<_>>();
-            distances.sort_by_key(|p| p.1);
+    for x in (-1..=(width+1)) {
+        for y in (-1..=(height+1)) {
+            let mut distances = coords.clone();
+            distances.sort_by_key(|c| (x-(c.x as i32)).abs() + (y-(c.y as i32)).abs());
             if distances[0] != distances[1] {
-                //position_to_count
+                let entry = id_to_count.entry(distances[0].clone()).or_insert(0);
+                *entry += 1;
+                if y == -1 || x == width+1 || y == height+1 || x == 0 {
+                    infinite_ids.insert(distances[0].clone());
+                }
             }
         }
     }
 
-    42
+    let mut id_and_counts = id_to_count
+        .iter()
+        .filter(|p| !(infinite_ids.contains(p.0)))
+        .collect::<Vec<_>>();
+    id_and_counts.sort_by_key(|p| p.1);
+    println!("{:#?}", id_and_counts);
+    *id_and_counts.last().unwrap().1
 }
