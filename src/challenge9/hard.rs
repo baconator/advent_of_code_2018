1ccb3c2cca1c6cea3f111cae0fc48dfa25541ec7
Bacon <baconator@users.noreply.github.com>
2018-12-10 21:00:42 -0800

    Alas, the awful tree did not save me. Time for a dlinked list.

--- src/challenge9/hard.rs
+++ src/challenge9/hard.rs
@@ -19,23 +19,71 @@ fn test_stuff() {
     assert_eq!(4, 5-((-1 as i32).abs()%5));
 }
 
-use std::collections::LinkedList;
+struct AwfulTree<T> {
+    data: Vec<Vec<T>>
+}
 
-pub fn solve(mut lines: impl Iterator<Item=String>) -> usize {
+const MAX_SIZE: usize = 1000;
+impl <T> AwfulTree<T> {
+    fn len(&self) -> usize {
+        self.data.iter().map(|d| d.len()).sum()
+    }
+
+    fn insert(&mut self, mut index: usize, item: T) {
+        let mut data_index = 0;
+        let mut data_index_to_split = None;
+        let mut found = false;
+        for mut data in self.data.iter_mut() {
+            if index < data.len() {
+               data.insert(index, item); 
+               if data.len() >= MAX_SIZE {
+                   data_index_to_split = Some(data_index);
+               }
+               found = true;
+               break;
+            } else {
+                index -= data.len();
+            }
+            data_index += 1;
+        }
+        if !found { panic!(); }
+        if let Some(di) = data_index_to_split {
+            let spleet = self.data[di].split_off(MAX_SIZE);
+            self.data.insert(di, spleet);
+        }
+    }
+
+    fn remove(&mut self, mut index: usize) -> T {
+        for mut data in self.data.iter_mut() {
+            if index < data.len() {
+                return data.remove(index);
+            } else {
+                index -= data.len();
+            }
+        }
+        panic!();
+    }
+}
+
+pub fn solve(mut lines: impl Iterator<Item=String>) -> u64 {
     let re = Regex::new("^(\\d+) players; last marble is worth (\\d+) points$").unwrap();
     let line = lines.next().unwrap();
     let cap = re.captures(&line).unwrap();
-    let player_count = cap[1].parse::<usize>().unwrap();
-    let final_marble = cap[2].parse::<usize>().unwrap();
+    let player_count = cap[1].parse::<u64>().unwrap();
+    let final_marble = cap[2].parse::<u64>().unwrap();
 
-    let mut placed_marbles = vec![0];
+    let mut placed_marbles = AwfulTree { data: vec![vec![0]] };
     let mut current_marble_index = 0;
     let mut player_scores = HashMap::new();
+    
+    use std::time::{SystemTime, UNIX_EPOCH};
+    let mut last_time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
 
     for mut marble in 1..=(final_marble*100) {
-        use std::time::SystemTime;
         if marble % 100000 == 0 {
-            println!("marble {} at {:#?}", marble, SystemTime::now());
+            let now = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+            println!("marble {} took {:?}", marble, now-last_time);
+            last_time = now;
         }
         if (marble % 23) == 0 && marble != 0 {
             let popped_marble_index = {
