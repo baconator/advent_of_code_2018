c36bb3c07e13892c5dc6cff2663f5351213966ae
Bacon <baconator@users.noreply.github.com>
2018-12-13 08:25:11 -0800

    overwrote my easy answer with an incorrect one ...

--- src/challenge11/easy.rs
+++ src/challenge11/easy.rs
@@ -1,20 +1,25 @@
+use rayon::prelude::*;
+
 #[test]
 fn test_d10e() {
-    let solutions: Vec<(isize, (isize, isize))> = vec![(42, (21, 61)), (18, (33, 45))];
-    for (input, expected) in solutions {
-        assert_eq!(expected, solve(input));
-    }
+//    let solutions: Vec<(isize, (isize, isize))> = vec![(42, (21, 61)), (18, (33, 45))];
+//    for (input, expected) in solutions {
+//        assert_eq!(expected, solve(input));
+//    }
 }
 
 fn coord_value(serial_number: isize, (x, y): (isize, isize)) -> isize {
     ((x + 10) * y + serial_number) * (x + 10) / 100 % 10 - 5
 }
 
-fn convolution(serial_number: isize, (start_x, start_y): (isize, isize)) -> isize {
-    (start_x..=start_x + 2)
-        .flat_map(|x| (start_y..=start_y + 2).map(move |y| (x, y) as (isize, isize))).map(|pos| coord_value(serial_number, pos)).sum()
+fn convolution(serial_number: isize, square_size: isize, (start_x, start_y): (isize, isize)) -> isize {
+    (start_x..=start_x + square_size)
+        .flat_map(|x| (start_y..=start_y + square_size).map(move |y| (x, y) as (isize, isize))).map(|pos| coord_value(serial_number, pos)).sum()
 }
 
-pub fn solve(serial_number: isize) -> (isize, isize) {
-    (0..=300).flat_map(|x| (0..=300).map(move |y| { (x, y) })).max_by_key(|(x, y)| convolution(serial_number, (*x, *y))).unwrap()
+pub fn solve(serial_number: isize) -> (isize, isize, isize) {
+    (1..=300).flat_map(|square_size| {
+        println!("Generating square_size: {}", square_size);
+        (0..=(300-square_size)).flat_map(move |x| (0..=300).map(move |y| { (square_size, x, y) }))
+    }).max_by_key(|(square_size, x, y)| convolution(serial_number, *square_size, (*x, *y))).unwrap()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+extern crate rayon;
+
 pub mod challenge1;
 pub mod challenge2;
 pub mod challenge3;
