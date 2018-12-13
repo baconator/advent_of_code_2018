54e6297129a9d06f47ea9d242d35c629e5604fa7
Bacon <baconator@users.noreply.github.com>
2018-12-13 06:53:31 -0800

    Ok ... lets try this on a real input.

--- src/challenge10/easy.rs
+++ src/challenge10/easy.rs
@@ -1,3 +1,9 @@
+extern crate itertools;
+extern crate regex;
+
+use self::itertools::Itertools;
+use self::regex::Regex;
+
 #[test]
 fn test_d10e() {
     let solutions = vec![include_str!("../../inputs/10t")];
@@ -6,42 +12,86 @@ fn test_d10e() {
     }
 }
 
-extern crate regex;
-use self::regex::Regex;
-
 type PositionAndVelocity = ((isize, isize), (isize, isize));
+
 fn next_positions(old: &Vec<PositionAndVelocity>) -> Vec<PositionAndVelocity> {
     old.iter().map(|((x_pos, y_pos), (x_vel, y_vel))| {
         ((x_pos + x_vel, y_pos + y_vel), (*x_vel, *y_vel))
     }).collect()
 }
 
-extern crate itertools;
-use self::itertools::Itertools;
-
 fn score_positions(pos_and_vels: &Vec<PositionAndVelocity>) -> usize {
     let mut in_line = 0;
     let all_positions = &pos_and_vels.iter().map(|(p, _)| p).group_by(|(x, _)| x);
     for (_, positions) in all_positions {
-        let mut sorted_positions = positions.collect::<Vec<_>>();
+        let mut sorted_positions = positions.map(|(_, y)| y).collect::<Vec<_>>();
         sorted_positions.sort_unstable();
-        let mut 
-        for (_, y) in sorted_positions.iter() {
-            
+        for y in 0..sorted_positions.len() {
+            let before = if y == 0 { None } else { sorted_positions.get(y - 1) };
+            let current = sorted_positions[y];
+            let after = sorted_positions.get(y + 1);
+            let mut add = false;
+            if let Some(v) = before {
+                if **v + 1 == *current {
+                    add = true;
+                }
+            }
+            if let Some(v) = after {
+                if **v - 1 == *current {
+                    add = true;
+                }
+            }
+            if add {
+                in_line += 1;
+            }
         }
     }
     in_line
 }
 
+use std::collections::hash_set::HashSet;
+
+fn print_state(pos_and_vels: &Vec<PositionAndVelocity>) {
+    let min_y = pos_and_vels.iter().map(|((_, y), _)| y).min().unwrap();
+    let min_x = pos_and_vels.iter().map(|((x, _), _)| x).min().unwrap();
+    let max_y = pos_and_vels.iter().map(|((_, y), _)| y).max().unwrap();
+    let max_x = pos_and_vels.iter().map(|((x, _), _)| x).max().unwrap();
+
+    let positions = pos_and_vels.iter().map(|(pos, _)| pos).collect::<HashSet<_>>();
+    for y in *min_y..=*max_y {
+        for x in *min_x..=*max_x {
+            if positions.contains(&(x, y)) {
+                print!("#");
+            } else {
+                print!(".");
+            }
+        }
+        print!("\n");
+    }
+}
+
 pub fn solve(mut lines: impl Iterator<Item=String>) {
     let re = Regex::new("^position=<\\s*(-?)(\\d+),\\s*(-?)(\\d+)> velocity=<\\s*(-?)(\\d+),\\s*(-?)(\\d+)>$").unwrap();
-    let mut positions = lines.map(|line| {
+    let mut positions_and_velocities = lines.map(|line| {
         let caps = re.captures(&line).unwrap();
         (
             ((caps[1].to_owned() + &caps[2]).parse::<isize>().unwrap(), (caps[3].to_owned() + &caps[4]).parse::<isize>().unwrap()),
             ((caps[5].to_owned() + &caps[6]).parse::<isize>().unwrap(), (caps[7].to_owned() + &caps[8]).parse::<isize>().unwrap())
         )
     }).collect::<Vec<_>>();
-    positions.sort_unstable();
-    println!("{:#?}", score_positions(&positions));
+    positions_and_velocities.sort_unstable();
+    let total = positions_and_velocities.len();
+    println!("total: {}", total);
+    let mut current_positions_and_velocities = positions_and_velocities.clone();
+    for _ in 0..10000 {
+        if score_positions(&current_positions_and_velocities) > 3 {
+            use std::io;
+
+            print_state(&current_positions_and_velocities);
+            let mut input = String::new();
+            io::stdin().read_line(&mut input).unwrap();
+        }
+        current_positions_and_velocities = next_positions(&current_positions_and_velocities);
+        assert_eq!(total, current_positions_and_velocities.len());
+    }
 }
