114b023bf34dd414585c8c725bc1e5dbcffa3272
Bacon <baconator@users.noreply.github.com>
2018-12-14 09:10:38 -0800

    Doing parsing logic.

--- src/challenge12/easy.rs
+++ src/challenge12/easy.rs
@@ -29,6 +29,40 @@ fn test_d12e() {
     }
 }
 
+enum Plant {
+    Alive,
+    Dead
+}
+
+fn parse_input(c: char) -> Option<Plant> {
+    if c == '#' {
+        Some(Plant::Alive)
+    } else if c == '.' {
+        Some(Plant::Dead)
+    } else {
+        panic!()
+    }
+}
+
 pub fn solve(mut lines: impl Iterator<Item=String>) -> usize {
+    let initial_state_re = Regex::new("^initial state: ([#|\\.]+)$").unwrap();
+    let pattern_re = Regex::new("^([#|\\.]+) => ([#|\\.])$").unwrap();
+    let initial_state_line = lines.next().unwrap();
+    let initial_state = initial_state_re
+        .captures(&initial_state_line)
+        .unwrap()[1]
+        .chars()
+        .filter_map(parse_input)
+        .collect::<Vec<_>>();
+    lines.map(|line| {
+        let cap = pattern_re.captures(&line).unwrap();
+        let left = cap[1].chars().filter_map(parse_input).collect::<Vec<_>>();
+        let right = if cap[2].chars().next().unwrap() == '#' {
+            Plant::Alive
+        } else {
+            Plant::Dead
+        };
+        42
+    });
     42
 }
